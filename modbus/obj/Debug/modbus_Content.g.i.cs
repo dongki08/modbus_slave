17c2b41d32f549f8f0625649ea7b1ea287f2020c
dongki <ehdgusdl0816@gmail.com>
2025-08-10 20:53:53 +0900

    250810 test1

--- modbus/obj/Debug/modbus_Content.g.i.cs
+++ modbus/obj/Debug/modbus_Content.g.i.cs
@@ -8,13 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fonts/fontawesome.otf")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fonts/fontawesome-webfont.ttf")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fonts/fontawesome-webfont.eot")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fonts/fontawesome-webfont.woff")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fonts/fontawesome-webfont.woff2")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("content/font-awesome.css")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("content/font-awesome.min.css")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("fonts/fontawesome-webfont.svg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("obj/debug/modbus_markupcompile.lref")]
 
 
